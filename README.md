1ec833a782baad649f9e903cc599ffcd0045332d
wangshumin <wang.shumin@bluebank.com.cn>
2019-04-10 17:04:41 +0800

    add code and okhttp git hub web address

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # OkHttpTemp
 okhttp的使用相关
+https://square.github.io/okhttp/
+https://github.com/square/okhttp.git
+https://github.com/square/okhttp/wiki/Interceptors
+
+
+使用okhttp 下载图片。
+使用okhttp 断点下载图片，到指定目录和imageView
+使用okhttp 同时断点下载多个图片，到指定目录和ImageView. + 进度条
+
+OkHttp解析
+
+OkHttpClient  newCall()--> new RealCall(client,request) 封装client和request, 然后调用RealCall equeue 传递Callback 
+
+RealCall (AsyncCall extends NamedRunable)  -> enqueue(Callback) -->然后用AsyncCall封装Callback 传递给Dispather equeue，
+
+Dispather  equeue   ThreadPoolExecutor
+readyAsyncQueue添加一个AysncCall,包含readyAsyncQueue 和runningAsyncQueue分别保存准备的AsyncCall和正在运行的AsyncCall
+然后遍历ready队列，有等待的，取出队列，放到runing队列里，然后遍历executable队列，
+回调AsyncCall 的executeOn（ThreadPoolExecutor）方法，线程池执行execute(this)执行AsyncCall,回调AsyncCall的execute()方法
+
+获取网络的Reponse， 然后回调AsyncCall 封装的callback，回调到HttpClient的回调方法
--- app/src/main/java/tech/xiaosuo/com/okhttptemp/MainActivity.java
+++ app/src/main/java/tech/xiaosuo/com/okhttptemp/MainActivity.java
@@ -1,11 +1,31 @@
 package tech.xiaosuo.com.okhttptemp;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.Message;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -17,14 +37,25 @@ import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private static final String TAG = "MainActivity";
+    Button downLoadButton;
+    ProgressBar progressBar;
+    ImageView imageView;
+    String url = "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1554272776682&di=70291487c5a10b49618a860b4535cf6a&imgtype=0&src=http%3A%2F%2Fimg.kutoo8.com%2Fupload%2Fimage%2F92246689%2F31698-ElPlsj_960x540.jpg";
+    Context mContext;
+    private static final int MSG_ERROR = -1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        postFormDataAsync();
+        downLoadButton = findViewById(R.id.download_button);
+        downLoadButton.setOnClickListener(this);
+        progressBar = findViewById(R.id.progressbar);
+        imageView = findViewById(R.id.image_view);
+        mContext = this;
+      //  postFormDataAsync();
         //getMethodAsync();
     //    new Thread(getSyncRunnable).start();
     }
@@ -152,4 +183,173 @@ public class MainActivity extends AppCompatActivity {
             getMethodSync();
         }
     };
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id){
+            case R.id.download_button:
+                downLoadFile(url);
+                break;
+                default:
+                    break;
+        }
+    }
+
+    private void downLoadFile(String url){
+/*           boolean checkPermission = requestWritePerm();
+           if(!checkPermission){
+                Log.d(TAG," we can't get the wirte external permission");
+                return;
+           }*/
+          OkHttpClient okHttpClient = new OkHttpClient();
+          Request request = new Request.Builder().url(url).build();
+         okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.d(TAG," onFailure e " + e.toString());
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                Log.d(TAG," onResponse e " + response.code() + " msg: " + response.message() );//+ " body: " + response.body().string()
+                byte[] imgBytes = response.body().bytes();
+               // Bitmap bitmap = BitmapFactory.decodeByteArray(imgBytes,0,imgBytes.length);
+                String sdPath = Environment.getExternalStorageDirectory().getAbsolutePath();
+                Log.d(TAG," sdpath: " + sdPath);
+                File file = new File(sdPath + File.separator + "meinv");
+                File pic = null;
+                try {
+                    if(!file.exists()){
+                        file.mkdirs();
+
+                    }
+                    pic = new File(file.getAbsolutePath() + "mei.jpg");
+                    boolean result = pic.createNewFile();
+                    if(result){
+                         Log.d(TAG," create file success");
+                    }else{
+                        Log.d(TAG," create file fail");
+                        Toast.makeText(getApplicationContext(),"create file fail",Toast.LENGTH_SHORT).show();
+                    }
+                    OutputStream outputStream = new FileOutputStream(pic);
+                    outputStream.write(imgBytes);
+                }catch (Exception e){
+                   // Toast.makeText(getApplicationContext(),e.toString(),Toast.LENGTH_SHORT).show();
+                    Log.d(TAG," exception: " + e.toString());
+                    Message msg = new Message();
+                    msg.what = MSG_ERROR;
+                    Bundle bundle = new Bundle();
+                    bundle.putString("exception",e.toString());
+                    msg.setData(bundle);
+                    mainHandler.sendMessage(msg);
+                    return;
+                }
+
+                Message msg = new Message();
+                msg.what = 0;
+              //  msg.obj = bitmap;
+                Bundle bundle = new Bundle();
+                bundle.putByteArray("bitmap_array",imgBytes);
+                msg.setData(bundle);
+                mainHandler.sendMessage(msg);
+
+            }
+        });
+    }
+
+    Handler mainHandler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            int what = msg.what;
+            Bundle bundle = msg.getData();
+            switch (what){
+                case 0:
+                    if(bundle == null){
+                        return;
+                    }
+                    byte[] bytes = bundle.getByteArray("bitmap_array");
+                    Bitmap bitmap = BitmapFactory.decodeByteArray(bytes,0,bytes.length);
+
+                    imageView.setImageBitmap(bitmap);
+                    break;
+                case MSG_ERROR:
+                    if(bundle == null){
+                        return;
+                    }
+                    String exception = bundle.getString("exception");
+                    Log.d(TAG," msg exception: " + exception);
+                    AlertDialog.Builder builder = new AlertDialog.Builder(mContext).setMessage(exception).setTitle("Warning").setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    }).setCancelable(true);
+                    AlertDialog alertDialog = builder.create();
+                    alertDialog.show();
+                    break;
+                    default:
+                        break;
+            }
+        }
+    };
+
+
+
+       @TargetApi(Build.VERSION_CODES.M)
+       private boolean requestWritePerm() {
+
+           if(Build.VERSION.SDK_INT < Build.VERSION_CODES.M){
+               Log.d(TAG," sdk int less than m");
+               return true;
+           }
+
+           if(checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+               Log.d(TAG," app have get the write external permission");
+               return true;
+           }
+           boolean showPermRat = shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+           if(showPermRat){
+               Log.d(TAG," should show permission Rationale");
+               requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},23);
+           }else{
+               Log.d(TAG," should not show permission Rationale");
+               requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},23);
+           }
+
+           return false;
+/*        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            return true;
+        }
+        if (checkSelfPermission() == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        }
+        if (shouldShowRequestPermissionRationale(READ_CONTACTS)) {
+          Snackbar.make(mPhoneNumberView, R.string.permission_rationale, Snackbar.LENGTH_INDEFINITE)
+                    .setAction(android.R.string.ok, new View.OnClickListener() {
+                        @Override
+                        @TargetApi(Build.VERSION_CODES.M)
+                        public void onClick(View v) {
+                            requestPermissions(new String[]{READ_CONTACTS}, REQUEST_READ_CONTACTS);
+                        }
+                    });
+        } else {
+            requestPermissions(new String[]{READ_CONTACTS}, REQUEST_READ_CONTACTS);
+        }*/
+
+    }
+
+    /**
+     * Callback received when a permissions request has been completed.
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode == 23) {
+            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //populateAutoComplete();
+                Log.d(TAG," we have get the write external permission");
+            }
+        }
+    }
 }
